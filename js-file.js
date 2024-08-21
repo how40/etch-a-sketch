31e0fdf53e407a1756f35d01b3640c0cf9d59986
how40 <99495489+how40@users.noreply.github.com>
2024-08-21 13:42:19 -0400

    dynamic sized grid squares

--- js-file.js
+++ js-file.js
@@ -1,7 +1,11 @@
+const body = document.querySelector("body")
 const container = document.querySelector("#container")
 container.setAttribute("class","flex-container")
 
+
 let createGrid = size => {
+
+    const squareSize = (500 / size);
 
     for (n=0;n<size;n++) { //how many rows
 
@@ -12,11 +16,14 @@ let createGrid = size => {
             
             const square = document.createElement('div');
             square.setAttribute("class","flex-square");
-
+            
+            square.style.width = `${squareSize}px`;
+            square.style.height = `${squareSize}px`;
+            
+            //pen trail
             square.addEventListener("mouseover", () => {
-                square.setAttribute("style","background-color:blue")
+                square.style.backgroundColor = 'blue'
             });
-
 
             row.appendChild(square);
             container.appendChild(row);
@@ -25,8 +32,33 @@ let createGrid = size => {
     }
 
 }//EOF
- 
+
+let deleteGrid = () => {
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
+
+}//EOF
+
+
+let initialSize = 16;
+
+createGrid(initialSize); //base grid
+
+const input = document.querySelector('input');
+const button = document.querySelector('button');
+
+button.addEventListener('click', () => {
+    const inputSize = input.value;
+    if (!Number.isInteger(parseInt(inputSize))) {
+        alert("Not a number!");
+    } else if (parseInt(inputSize) > 101) {
+        alert("Too big!");
+    } else { 
+        deleteGrid();
+        createGrid(inputSize);
+    }
+});
 
 
 
-createGrid(16);
